b790b5d7440f1631bd138861f17a6466adcd5c8f
Nattapong Rodmuang <nattapong.r@forth.co.th>
2014-05-20 12:28:36 +0700

    Release download file example

---
+++ DownloadFile/frmMain.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace DownloadFile
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.pbDownload = new System.Windows.Forms.ProgressBar();
+            this.txtUri = new System.Windows.Forms.TextBox();
+            this.rbtSync = new System.Windows.Forms.RadioButton();
+            this.rbtAsync = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(197, 39);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 0;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // pbDownload
+            // 
+            this.pbDownload.Location = new System.Drawing.Point(12, 39);
+            this.pbDownload.Name = "pbDownload";
+            this.pbDownload.Size = new System.Drawing.Size(179, 23);
+            this.pbDownload.TabIndex = 1;
+            // 
+            // txtUri
+            // 
+            this.txtUri.Location = new System.Drawing.Point(13, 13);
+            this.txtUri.Name = "txtUri";
+            this.txtUri.Size = new System.Drawing.Size(259, 20);
+            this.txtUri.TabIndex = 2;
+            // 
+            // rbtSync
+            // 
+            this.rbtSync.AutoSize = true;
+            this.rbtSync.Checked = true;
+            this.rbtSync.Location = new System.Drawing.Point(12, 73);
+            this.rbtSync.Name = "rbtSync";
+            this.rbtSync.Size = new System.Drawing.Size(81, 17);
+            this.rbtSync.TabIndex = 3;
+            this.rbtSync.TabStop = true;
+            this.rbtSync.Text = "Synchronus";
+            this.rbtSync.UseVisualStyleBackColor = true;
+            // 
+            // rbtAsync
+            // 
+            this.rbtAsync.AutoSize = true;
+            this.rbtAsync.Location = new System.Drawing.Point(105, 73);
+            this.rbtAsync.Name = "rbtAsync";
+            this.rbtAsync.Size = new System.Drawing.Size(86, 17);
+            this.rbtAsync.TabIndex = 4;
+            this.rbtAsync.TabStop = true;
+            this.rbtAsync.Text = "Asynchronus";
+            this.rbtAsync.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 102);
+            this.Controls.Add(this.rbtAsync);
+            this.Controls.Add(this.rbtSync);
+            this.Controls.Add(this.txtUri);
+            this.Controls.Add(this.pbDownload);
+            this.Controls.Add(this.btnDownload);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Download File";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.ProgressBar pbDownload;
+        private System.Windows.Forms.TextBox txtUri;
+        private System.Windows.Forms.RadioButton rbtSync;
+        private System.Windows.Forms.RadioButton rbtAsync;
+    }
+}
+
---
+++ DownloadFile/frmMain.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Diagnostics;
+
+namespace DownloadFile
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            if (GetFileNameFromUrl(txtUri.Text) == "")
+            {
+                return;
+            }
+
+            if (rbtSync.Checked)
+            {
+                try
+                {
+                    WebClient webClient = new WebClient();
+                    webClient.DownloadFile(new Uri(txtUri.Text), GetFileNameFromUrl(txtUri.Text));
+                    MessageBox.Show("Download Complete (Synchrouns)", "Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Process.Start(Application.StartupPath);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else if (rbtAsync.Checked)
+            {
+                try
+                {
+                    WebClient webClient = new WebClient();
+                    webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                    webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                    webClient.DownloadFileAsync(new Uri(txtUri.Text), GetFileNameFromUrl(txtUri.Text));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            pbDownload.Value = e.ProgressPercentage;
+        }
+
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Download Complete (Asynchrouns)", "Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            pbDownload.Value = 0;
+            Process.Start(Application.StartupPath);
+        }
+
+        private string GetFileNameFromUrl(string url)
+        {
+            string[] token = url.Split('/');
+            if (token.Length > 0)
+            {
+                return token[token.Length - 1];
+            }
+            MessageBox.Show("Can't get file name.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return "";
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
